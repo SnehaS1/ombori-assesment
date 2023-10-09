1d6605c813a22093ea5ed6e152a2e2cc1ab54404
Sneha S <snehasushatha@gmail.com>
2023-10-10 00:25:12 +0400

    infinite scrolling

--- src/components/GetUsers.tsx
+++ src/components/GetUsers.tsx
@@ -1,62 +1,86 @@
 import React from "react";
 import {
-    useQuery,
+    useInfiniteQuery
 } from '@tanstack/react-query'
-
-import Image from 'react-bootstrap/Image';
+import InfiniteScroll from "react-infinite-scroller";
+import Navbar from 'react-bootstrap/Navbar';
 
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
 import { get } from 'lodash';
-import { UserListGlass, UserListWrapperGlass } from "../styles";
 
 
-interface Users {
-    id: number;
-    email: string;
-    first_name: string;
-    last_name: string;
-    avatar: string;
-}
 
 const GetUsers = () => {
 
-    const { isLoading, error, data } = useQuery<Users[], Error>({
-        queryKey: ['usersData'],
-        queryFn: () =>
-            fetch('https://reqres.in/api/users').then(res => res.json()),
+    const fetchPosts = async ({ pageParam = 1 }) => {
+        const response = await fetch(
+            `https://reqres.in/api/users/?page=${pageParam}`
+        );
+        const results = await response.json();
+        return {
+            results,
+            nextPage: pageParam + 1,
+            totalPages: results.total_pages + 1
+        };
+    };
+    const {
+        data,
+        isLoading,
+        isError,
+        fetchNextPage,
+        hasNextPage,
+    } = useInfiniteQuery(["usersData"], fetchPosts, {
+        getNextPageParam: (lastPage, pages) => {
+            if (lastPage.nextPage < lastPage.totalPages) return lastPage.nextPage;
+            return undefined;
+        }
     });
 
-    if (isLoading) return <div>Fetching posts...</div>;
-    if (error) return <div>An error occurred: {error.message}</div>;
-    const users: Users[] = get(data, 'data', []);
-    console.log('posts', users, data);
+    if (isLoading) return <div>Fetching users...</div>;
+    if (isError) return <div>An error occurred</div>;
 
     return (
-        <ul>
-            <Container fluid>
-                {users.map((post: any) => (
+        <div>
 
-                    <UserListWrapperGlass fluid="md" className=" mb-9" key={get(post, 'id')}>
-
-                        <Col sm={2} xs={5} className="border-orange-600">
-                            <Image src={get(post, 'avatar', '')}
-                                alt={get(post, 'avatar', '')} roundedCircle />
-                        </Col>
-                        <Col sm={10} className="flex items-center">
-                            <UserListGlass>
-
-                                <p className="font-sans md:font-serif text-left border-blue-200 h-3/5">{get(post, 'first_name') + ' ' + get(post, 'last_name')}</p>
-                                <p className=" d-none d-sm-block font-sans md:font-serif text-left border-blue-200 h-3/5">{get(post, 'email')}</p>
-
-                            </UserListGlass>
-                        </Col>
-                    </UserListWrapperGlass>
+            <Navbar expand="lg" className="bg-body-tertiary" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#">Users</Navbar.Brand>
+                </Container>
+            </Navbar>
 
 
-                ))}
+            <Container fluid className="mb-3">
+                {/* <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage}> */}
+                <InfiniteScroll hasMore={hasNextPage} loadMore={() => fetchNextPage()}>
+
+                    {get(data, 'pages', [])
+                        .map((page) => get(page, 'results.data', [])
+                            .map((post: any) => (
+                                <div className="card mt-3 py-2 " key={get(post, 'id')}>
+                                    <div className="row no-gutters">
+                                        <div className="col-md-4  flex items-center justify-center">
+                                            <img src={get(post, 'avatar', '')} style={{ height: '200px', width: '200px', borderRadius: '50%' }}
+                                                alt={get(post, 'avatar', '')} className="card-img" />
+                                        </div>
+                                        <div className="col-md-8 flex items-center justify-center ">
+                                            <div className="card-body">
+                                                <h3 className="card-title">{`${get(post, 'first_name')} ${get(post, 'last_name')}`}</h3>
+                                                <p className="card-text">{get(post, 'email')}</p>
+                                                {/* <p className="card-text"><small className="text-muted">{get(post, 'email')}</small></p> */}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            )))}
+                    {isLoading && <div>Wait there's more...</div>}
+
+                    {!hasNextPage && <div className="my-3 text-2xl " style={{ textShadow: ' 4px 4px 6px rgba(0,0,0,0.6)', boxShadow: ' 10px 12px 15px -2px rgba(0,0,0,0.4)' }}>End of page...No more data found</div>}
+                </InfiniteScroll>
+
             </Container>
-        </ul>
+
+        </div>
     );
 };
 
